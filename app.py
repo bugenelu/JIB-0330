4e6de05b0bcecf273e7b64b443240b974c4736e1
Brock Smith <brocksmith225@gatech.edu>
2021-01-21 11:27:14 -0500

    Added sample for Jinja templating and Flask application to serve the template

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 # Initialize the Flask application
 app = Flask(__name__, template_folder='pages')
 
 
+# Sample class
+class Sample():
+
+	name = 'Sample'
+	shouldShowSecret = True
+	secret = 'shhh... don\'t tell'
+	values = [0, 1, 1, 2, 3, 5, 8, 13]
+
+
 # Maps url extension '/' to this function
 @app.route('/')
 def hello():
 
-	# Returns just the text 'Hello, World!', to be replaced with an HTML template
-    return 'Hello, World!'
+	# Creates a sample variable to pass to the HTML template
+	sample_var = 'This variable holds a single string'	
+
+	# Creates a sample object to pass to the HTML template
+	sample_obj = Sample()
+
+	# Returns the index.html template with the given values
+	return render_template("index.html", sample_var=sample_var, sample_obj=sample_obj)
 
 
-# Run the application
-app.run(host="127.0.0.1", port=8080, debug=True)
+# Run the application on the specified IP address and port
+app.run(host="127.0.0.1", port=80, debug=True)
