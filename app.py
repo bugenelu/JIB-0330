c8ad56e81e7249c41ed098ae9ac8e2e14ace0ad1
Brock Smith <brocksmith225@gatech.edu>
2021-01-28 11:46:58 -0500

    Added a basic HTML template and Flask functions to fill in an HTML template with data from the sample Twine story JSON

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os, json
 
 
 # Initialize the Flask application
@@ -19,13 +20,66 @@ class Sample():
 def hello():
 
 	# Creates a sample variable to pass to the HTML template
-	sample_var = 'This variable holds a single string'	
+	sample_var = 'This variable holds a single string'
 
 	# Creates a sample object to pass to the HTML template
 	sample_obj = Sample()
 
 	# Returns the index.html template with the given values
-	return render_template("index.html", sample_var=sample_var, sample_obj=sample_obj)
+	return render_template("index.html", sample_var=sample_var, sample_obj=sample_obj, sample_story="data")
+
+
+# Serves the root page of the specified story
+@app.route('/story/<story>')
+def story_root(story):
+
+	# Creates file path to the story's JSON file
+	filepath = os.path.join('twine_ingest', story + '.json')
+
+	# Checks whether or not the story exists
+	if not os.path.exists(filepath):
+		# TODO: return an error page
+		pass
+
+	# Opens the JSON file corresponding to the story
+	with open(filepath) as story_json:
+
+		# Converts text of file into JSON dictionary
+		story_data = json.load(story_json)
+
+		# Gets the root page's page ID
+		page_id = story_data['root-ID']
+
+		# Gets the page data for the specified page ID
+		page = story_data['page-nodes'][page_id]
+
+		# Returns the story_page.html template with the specified page
+		return render_template("story_page.html", story=story, page=page)
+
+
+# Serves the specified page of the specified story
+@app.route('/story/<story>/<page_id>')
+def story_page(story, page_id):
+
+	# Creates file path to the story's JSON file
+	filepath = os.path.join('twine_ingest', story + '.json')
+
+	# Checks whether or not the story exists
+	if not os.path.exists(filepath):
+		# TODO: return an error page
+		pass
+
+	# Opens the JSON file corresponding to the story
+	with open(filepath) as story_json:
+
+		# Converts text of file into JSON dictionary
+		story_data = json.load(story_json)
+
+		# Gets the page data for the specified page ID
+		page = story_data['page-nodes'][page_id]
+
+		# Returns the story_page.html template with the specified page
+		return render_template("story_page.html", story=story, page=page)
 
 
 # Run the application on the specified IP address and port
