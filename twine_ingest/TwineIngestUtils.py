7a894d6c93cdcb0b5c06eb2dacc539df114aea20
mattfromatlanta <77545498+mattfromatlanta@users.noreply.github.com>
2021-01-27 21:39:11 -0500

    follow up tweaks to ingest script
    Small adjustments to improve handling of unneeded carriage returns in passage texts.

--- twine_ingest/TwineIngestUtils.py
+++ twine_ingest/TwineIngestUtils.py
@@ -1,16 +1,19 @@
 import re
 
 
+# prepends an import ID# to a string with a '-' delimiter
 def prepend_id(import_id, string_to_prepend):
     return str(import_id) + '-' + string_to_prepend
 
 
+# deletes returns at the end of a string
 def delete_end_returns(s):
     while s[-2:-1] == "\n":
         s = s[0:-2]
     return s
 
 
+# makes a page node from tw-passage tag contents
 def make_page_node(passage, import_id, id_dict):
     page_id = prepend_id(import_id, passage.attrs['pid'])
     page_name = prepend_id(import_id, passage.attrs['name'])
@@ -22,16 +25,18 @@ def make_page_node(passage, import_id, id_dict):
     return page_node
 
 
+# makes a dictionary of the passage's child nodes
 def make_child_dict(children, import_id, id_dict):
     child_dict = {}
     for child in children:
         page_name = prepend_id(import_id, child[child.find("->") + 2:])
         link_text = child[0: child.find("->")]
         page_id = id_dict[child[child.find("->") + 2:]]
-        child_dict[page_id] = {"child-name": page_name, "link-text": link_text, "child-ID": page_id}
+        child_dict[page_id] = {"child-name": page_name, "link-text": delete_end_returns(link_text), "child-ID": page_id}
     return child_dict
 
 
+# makes a dictionary of ['name' : 'pid'] k-v pairs. PID's are prepended with the import ID#
 def make_id_dict(passages, import_id):
     id_dict = {}
     for passage in passages:
