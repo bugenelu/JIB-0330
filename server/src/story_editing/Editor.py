bdf213b0cdd7ae89d72492bb6de2d6f04653693f
Fishel, Matthew Edward <mfishel7@gatech.edu>
2021-03-02 10:23:22 -0500

    small changes to Editor.py, StoryGraph.py to get going on Sprint 3

--- server/src/story_editing/Editor.py
+++ server/src/story_editing/Editor.py
@@ -1,10 +1,12 @@
 """
 Welcome to the Editor. The Editor is responsible for:
 
+    TODO: Are these tasks handled before the Editor is created?
     Reading the story content of a database.
     Rendering the content of a database for the Admin UI
         e.g. returning formatted lists of existing stories and story elements
-    Creating StoryGraph objects from JSON in a database or from UI input.
+    TODO: Implement the following...
+    Creating StoryGraph objects from database or from UI input.
     Editing and combining open StoryGraph objects, which includes creating new
         PageNode and ChildLink objects as needed.
     Rendering the current state (and data) of open StoryGraph objects for the Admin UI
@@ -29,15 +31,19 @@ from collections import deque
 from StoryGraph import StoryGraph
 
 
-# TODO: This class is a work-in-progress. See below...
+# TODO: how does the editor interact with the database?
 class Editor:
 
     # TODO: Define fields of Editor...
     # TODO: Syntax for how the database is specified?
     # TODO: determine specific data the Editor needs in order to read from database
     def __init__(self, database=None):
-        openStories = {}
+        """
+        :param database: a text string that lets this Editor find the database
+        openStories: a dictionary of stacks of StoryGraph objects
+        """
         database = None
+        openStories = {}
 
     # gets a list of stories for the Admin UI
     # TODO: Implement reading data
@@ -49,11 +55,16 @@ class Editor:
     # TODO: how many StoryGraphs per stack?
     # TODO: how is the story data specified and read from the database?
     def openStory(self, story):
+        """
+
+        :param story: A formatted string from the database with story contents (metadata, pages, links, etc...)
+        :return: 0 on success, -1 on failure
+        """
         story_graph = StoryGraph(story)
-        if story_graph.story_name not in self.openStories:
+        if story_graph.story_id not in self.openStories:
             story_stack = deque()
             story_stack.append(story_graph)
-            self.openStories[story_graph.story_name] = story_stack
+            self.openStories[story_graph.story_id] = story_stack
             return 0
         else:
             # story is already open and conflict resolution is needed.
@@ -65,15 +76,19 @@ class Editor:
 
     # TODO: test
     def connectTrees(self, parent_graph, child_graph, parent_node, link_text):
-        parentID = parent_graph.story_name
-        version = parent_graph.addSubtree(child_graph, parent_node, link_text)
-        if version == -1:
+        parentID = parent_graph.story_ID
+        newGraph = parent_graph.addSubtree(child_graph, parent_node, link_text)
+        if newGraph == -1:
             return -1
-        self.openStories[parentID].push(version)
+        self.openStories[parentID].push(newGraph)
+        return 0
+
+    def addNode(self, graph, new_node, parent_node, link_text):
+        graphID = graph.story_ID
+        newGraph = graph.addNode(new_node, parent_node, link_text)
+        if newGraph == -1:
+            return -1
+        self.openStories[graphID].push(newGraph)
         return 0
 
     # TODO: Define two page deletes-- one removes a single node. one removes a node and all of its descendants.
-    # TODO: consider bidirectional edges.
-
-
-
