c1b8ae031a8d40a233908ebb26fe711930f1485e
Brock Smith <brocksmith225@gatech.edu>
2021-04-14 13:36:52 -0400

    Added minimum length for password

--- static/scripts/confirm_password.js
+++ static/scripts/confirm_password.js
@@ -2,7 +2,11 @@ $(function() {
 	$("input[type=submit]").click(function(event) {
 		event.preventDefault();
 		if ($("input[name=password]").val() == $("input[name=confirm-password]").val()) {
-			$(this).parent().submit();
+			$(this).parent().validate();
+			console.log($(this).parent().valid());
+			if ($(this).parent().valid()) {
+				// $(this).parent().submit();
+			}
 		} else {
 			$("input[name=\"confirm-password\"]")[0].setCustomValidity("Passwords must match");
 			$("input[name=\"confirm-password\"]")[0].reportValidity();
